524539f25f8b2fdfab62d9e9c53204a161384402
sandeep <mailto:support@abmtechno.com>
2024-03-03 19:12:02 +0530

    first commit

---
+++ src/main/java/org/example/controllers/TestController.java
@@ -0,0 +1,15 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String getHello()
+    {
+        return "Hello world";
+    }
+
+}
